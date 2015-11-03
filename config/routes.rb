7c534981125669dde39c0421a816519f84a76b91
Julius Farrington <survue@outlook.com>
2015-11-03 00:42:25 -0500

    Added ilovetocode route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'aboutus' => 'help#about'
 
+  get  'ilovetocode' => 'help#index' 
+
    # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
